9f10d276476b7388542a4b9b57b10f0606cd0ff6
ElizioMartins <elizio.martins@hotmail.com>
2021-02-12 08:18:48 -0300

    Ajustes

--- script.js
+++ script.js
@@ -37,19 +37,19 @@ function drawFood(){
 document.addEventListener('keydown', update);
 
 function update (event){
-    if(event.keyCode == 37 && direction != "rigth") direction = "left";
+    if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "rigth";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 
 }
 
 function iniciarJogo(){
 
-    if (snake[0]. > 15 * box && direction == "right") snake[0].x = 0;
-    if (snake[0]. < 0 && direction == "left") snake[0].x = 16 * box;
-    if (snake[0]. > 15 * box && direction == "down") snake[0].y = 0;
-    if (snake[0]. < 0 && direction == "up") snake[0].y = 16 * box;
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
     
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
@@ -57,7 +57,7 @@ function iniciarJogo(){
             alert('Game Over :(');
         }
     }
-
+   
 
     criarBG();
     criarCobrinha();
@@ -68,8 +68,8 @@ function iniciarJogo(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
     if(snakeX != food.x || snakeY != food.y){
         snake.pop();
